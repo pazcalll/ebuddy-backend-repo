359b7011b96d505417e2abdd179bf395c2d9b878
pazcalll <yazeedarifin@gmail.com>
2025-03-09 19:56:15 +0700

    feat: dotenv

--- core/app.ts
+++ core/app.ts
@@ -2,7 +2,6 @@ import express from "express";
 import userRoutes from "./../routes/userRoutes";
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
@@ -10,6 +9,4 @@ app.use("/", userRoutes);
 
 console.log(userRoutes);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+export default app;
