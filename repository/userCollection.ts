0c2031c95e4d62a1db138d270e5f7cb3f3648199
pazcalll <yazeedarifin@gmail.com>
2025-03-09 18:03:48 +0700

    feat: fetch user data

--- repository/userCollection.ts
+++ repository/userCollection.ts
@@ -1,26 +1,9 @@
 import { Request, Response } from "express";
 import { db } from "../config/firebaseConfig";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, getDocs } from "firebase/firestore";
 import { User } from "../entities/user";
 
 const postUserData = async (req: Request, res: Response) => {
-  if (!req.body)
-    res.status(400).send({
-      message: "Content can not be empty!",
-    });
-  if (!req.body.totalAverageWeightRatings)
-    res.status(400).send({
-      message: "Content can not be empty!",
-    });
-  if (!req.body.numberOfRents)
-    res.status(400).send({
-      message: "Content can not be empty!",
-    });
-  if (!req.body.recentlyActive)
-    res.status(400).send({
-      message: "Content can not be empty!",
-    });
-
   try {
     const user: Partial<User> = {
       totalAverageWeightRatings: req.body.totalAverageWeightRatings,
@@ -38,4 +21,24 @@ const postUserData = async (req: Request, res: Response) => {
   //   res.send("Posting user data");
 };
 
-export default postUserData;
+const fetchUserData = async (req: Request, res: Response) => {
+  try {
+    const users: User[] = [];
+    const querySnapshot = await getDocs(collection(db, "USERS"));
+    querySnapshot.forEach((doc) => {
+      const data: User = {
+        _id: doc.id,
+        totalAverageWeightRatings: doc.data().totalAverageWeightRatings,
+        numberOfRents: doc.data().numberOfRents,
+        recentlyActive: doc.data().recentlyActive,
+      };
+      users.push(data);
+    });
+    res.json(users);
+  } catch (e) {
+    console.error("Error fetching documents: ", e);
+    res.status(500).send({ message: "Error fetching documents" });
+  }
+};
+
+export { postUserData, fetchUserData };
--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -1,15 +1,13 @@
 import express, { Request, Response } from "express";
-import postUserData from "../repository/userCollection";
 import postMiddleware from "../middleware/postMiddleware";
+import { fetchUserData, postUserData } from "../repository/userCollection";
 
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
   res.send("Hello from user routes");
 });
-router.get("/fetch-user-data", (req: Request, res: Response) => {
-  res.send("Fetching user data");
-});
+router.get("/fetch-user-data", fetchUserData);
 router.get("/update-user-data", (req: Request, res: Response) => {
   res.send("Updating user data");
 });
