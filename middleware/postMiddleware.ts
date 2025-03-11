f360ca559cd156b110b0d748013d0b26cb2b9483
pazcalll <yazeedarifin@gmail.com>
2025-03-12 05:10:19 +0700

    feat: update postMiddleware

--- middleware/postMiddleware.ts
+++ middleware/postMiddleware.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { UserSchema } from "../entities/user";
 
 export default function postMiddleware(req: Request, res: Response, next: any) {
-  const validation = UserSchema.safeParse(req.body);
+  const validation = UserSchema.omit({ _id: true }).safeParse(req.body);
   if (!validation.success) {
     res.json(validation.error.flatten().fieldErrors);
     return;
