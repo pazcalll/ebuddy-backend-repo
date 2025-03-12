7a31406a5009eafffa09499ea264bae5e2778db9
pazcalll <yazeedarifin@gmail.com>
2025-03-12 11:26:48 +0700

    feat: add firebase function

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -1,9 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
-import admin from "firebase-admin";
+import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
+import admin, { auth } from "firebase-admin";
 import dotenv from "dotenv";
+import { connectAuthEmulator, getAuth } from "firebase/auth";
 
 dotenv.config();
 // TODO: Add SDKs for Firebase products that you want to use
@@ -11,7 +12,7 @@ dotenv.config();
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+let firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
@@ -19,6 +20,7 @@ const firebaseConfig = {
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID,
   measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
 };
 console.log(firebaseConfig);
 
@@ -26,12 +28,15 @@ console.log(firebaseConfig);
 const app = initializeApp(firebaseConfig);
 admin.initializeApp({
   credential: admin.credential.applicationDefault(),
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: firebaseConfig.projectId,
+  databaseURL: firebaseConfig.databaseURL,
 });
-// export const analytics = getAnalytics(app);
-// admin.appCheck();
 
 const db = getFirestore(app);
+
+// if (process.env.APP_MODE != "production") {
+//   connectFirestoreEmulator(db, "127.0.0.1", 8080);
+//   connectAuthEmulator(getAuth(), "http://127.0.0.1:9099");
+// }
 
 export { app, admin, db };
